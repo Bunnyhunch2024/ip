cab181054d754927b9471bd72ae9d679d6201590
Bunnyhunch2024 <wantuchang@gmail.com>
2026-03-06 16:29:42 +0800

    Level-0 commit

---
+++ src/main/java/Wan.java
@@ -0,0 +1,18 @@
+public class Wan {
+    private static final String line = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(line);
+        System.out.println("Hello I'm Wan \n");
+        System.out.println("What can I do for you? \n");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        }
+
+
+
+
+
+
+}
