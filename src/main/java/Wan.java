c1337cc46e9f40dceb5ae0bc08b393e1f9bef396
Bunnyhunch2024 <wantuchang@gmail.com>
2026-03-09 18:18:08 +0800

    level-2 commit

--- src/main/java/Wan.java
+++ src/main/java/Wan.java
@@ -7,6 +7,8 @@ public class Wan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] storage = new String[100];
+        int numbers = 0;
 
         printLine();
         System.out.println("    Hello I'm Wan");
@@ -20,11 +22,20 @@ public class Wan {
                 System.out.println("    Bye. Hope to see you again soon!");
                 printLine();
                 break;
+            }else if(input.equals("list")) {
+                printLine();
+                for (int i = 0; i < numbers; i += 1) {
+                    System.out.print("    " + (i + 1) + ": ");
+                    System.out.println(storage[i]);
+                }
+                printLine();
+            }else{
+                printLine();
+                storage[numbers] = input;
+                numbers += 1;
+                System.out.println("    added: " + input);
+                printLine();
             }
-            printLine();
-            System.out.println("    " + input);
-            printLine();
-
         }
 
         scanner.close();
